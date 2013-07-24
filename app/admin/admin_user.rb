089941d45bf865f448d23691f691b16e1a325b8e
Brian Howenstein <brian@hwrd.com>
2013-07-24 09:40:05 -0400

    Permit params in ActiveAdmin

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,4 +1,10 @@
 ActiveAdmin.register AdminUser do
+  controller do
+    def permitted_params
+      params.permit admin_user: [:email, :password, :password_confirmation]
+    end
+  end
+  
   index do
     column :email
     column :current_sign_in_at
--- app/admin/location.rb
+++ app/admin/location.rb
@@ -1,3 +1,7 @@
 ActiveAdmin.register Location do
-
+  controller do
+    def permitted_params
+      params.permit location: [:name, :address, :latitude, :longitude]
+    end
+  end
 end
