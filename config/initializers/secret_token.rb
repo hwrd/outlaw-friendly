c58efc2c318e0a102cc6c037f4d9cec76e8d14c7
Brian Howenstein <brian@hwrd.com>
2013-07-23 23:15:28 -0400

    Generate Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OutlawFriendly::Application.config.secret_key_base = '65625672f4d4a492822b39260472a6d69eed5bb6e6af83d397a86384a728bac7b12909c888a3676a6adee68b3fd4ee8bcec32b9aeb01fd5f5516e673fe57a781'
