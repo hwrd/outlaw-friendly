bf618f3fea42b49f22864a5c615f3e73ddcfc850
Brian Howenstein <brian@hwrd.com>
2013-07-25 10:53:13 -0400

    Revert "Add Location Resource"
    This reverts commit 03c55294733d2c5fcd4bd7114b4fee4ebed11f1b.
    
    Conflicts:
    	app/models/location.rb
    	config/routes.rb
    	db/schema.rb

--- app/models/location.rb
+++
@@ -1,7 +0,0 @@
-class Location < ActiveRecord::Base
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
-
-  validates :name, presence: true
-  validates :address, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 OutlawFriendly::Application.routes.draw do
-
-  resources :locations
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'locations#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
