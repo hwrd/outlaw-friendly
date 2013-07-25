474655a0803b40b49aa0e42d0215e23f74b34a14
Brian Howenstein <brian@hwrd.com>
2013-07-25 11:46:26 -0400

    Add Validations and Geocoding

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :website, presence: true
 end
